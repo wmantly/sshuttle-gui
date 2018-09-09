0018a91109b8f8c5588c0c9549a325cf398a9599
William Mantly <wmantly@gmail.com>
2018-09-09 17:25:10 -0400

    better bash

--- sshuttle_update.sh
+++ sshuttle_update.sh
@@ -4,13 +4,22 @@ install_dir="`pwd`/lib/python"
 PYVERSION="`python3 -c 'import sys; sys.stdout.write(sys.version[0:3])'`"
 PYTHONPATH=`python3 -c "import sys; sys.stdout.write(':'.join(sys.path[1::]))"`
 SITE_PATH="`pwd`/lib/python/lib/python$PYVERSION/site-packages"
-
-mkdir -p $SITE_PATH
+CLONE_DIR="tmp/sshuttle"
 
 export PYTHONPATH=$SITE_PATH:$PYTHONPATH
 
-git clone https://github.com/wmantly/sshuttle tmp/sshuttle
+if [ -d "$CLONE_DIR" ]; then
+  rm -rf "$CLONE_DIR"
+fi
 
-cd tmp/sshuttle
+git clone https://github.com/wmantly/sshuttle $CLONE_DIR
 
-python3 setup.py install --prefix=$install_dir
+if [ -d "$SITE_PATH" ]; then
+  rm -rf "$SITE_PATH"
+fi
+
+mkdir -p "$SITE_PATH"
+
+cd "$CLONE_DIR"
+
+python3 setup.py install --prefix="$install_dir"
